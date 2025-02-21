9f431c9d2f017e0a81f8af8b0ede28dd669dad66
APTIVMIND <aptivmind@gmail.com>
2025-02-21 21:59:07 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,19 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="piretry",  # Use a clean, simple name for PyPI
-    version="0.1.0",
+    version="0.1.1",
     author="APTIVMIND",
     author_email="aptivmind@gmail.com",  # Replace with a valid email
     description="A retry decorator for Python functions supporting both sync and async.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/APTIVMIND/piretry",  # Replace with the actual repo link
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.12",
 )
